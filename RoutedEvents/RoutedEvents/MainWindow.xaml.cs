258525239582c369d6c6d77a83dcbfa6186992c3
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-16 08:44:18 +1100

    Demo of Routed Events - Direct, Bubbling and Tunneling

---
+++ RoutedEvents/RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Grid grid = new Grid();
+            this.Content = grid;
+
+            Button btn = new Button();
+            btn.Content = "Click me";
+            btn.PreviewMouseLeftButtonDown += Button_PreviewMouseLeftButtonDown;
+            btn.PreviewMouseRightButtonUp += Button_PreviewMouseRightButtonUp;
+            btn.Width = 150;
+            btn.Height = 100;
+
+            grid.Children.Add(btn);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked - direct event");
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse button went up - bubbling");
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse button went up - tunneling");
+        }
+
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Left mouse button went down - tunneling");
+        }
+
+        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Right mouse button went up - tunneling");
+        }
+    }
+}
