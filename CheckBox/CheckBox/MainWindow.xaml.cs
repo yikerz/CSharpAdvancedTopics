93a4766ce715e8559871504f157fa1c415ed566f
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-17 11:11:00 +1100

    Demo of check box usage

---
+++ CheckBox/CheckBox/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void cbAllCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVal = cbAllTopings.IsChecked == true;
+            cbSalami.IsChecked = newVal;
+            cbMushroom.IsChecked = newVal;
+            cbMozzarella.IsChecked = newVal;
+        }
+        private void cbSingleCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbAllTopings.IsChecked = null;
+            if((cbSalami.IsChecked == true) && (cbMozzarella.IsChecked == true) && (cbMushroom.IsChecked == true))
+            {
+                cbAllTopings.IsChecked = true;
+            }
+            if ((cbSalami.IsChecked == false) && (cbMozzarella.IsChecked == false) && (cbMushroom.IsChecked == false))
+            {
+                cbAllTopings.IsChecked = false;
+            }
+        }
+
+    }
+}
