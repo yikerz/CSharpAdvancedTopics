2ca9bfba65259e49ca31630aea93044e46419cbc
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-18 13:48:55 +1100

    Demo of small project - Todo List (without database connection)

---
+++ TodoApp/TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTodoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string TodoInput = tbToDoInput.Text;
+            TextBlock newTodoItem = new TextBlock
+            {
+                Text = TodoInput,
+                Foreground = new SolidColorBrush(Colors.White),
+                Margin = new Thickness(10)
+            };
+            spTodoList.Children.Add(newTodoItem);
+            tbToDoInput.Clear();
+        }
+    }
+}
