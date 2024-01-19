72e6366e758e129b0e03b7a152fa95ce881fdbfe
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-19 15:12:46 +1100

    Another data binding demo using Person class with Name and Age properties

---
+++ DataBindingDemo2/DataBindingDemo2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DataBindingDemo2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingDemo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person
+        {
+            Name = "Travis",
+            Age = 32,
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = person.Name + " is " + person.Age + " yo.";
+            MessageBox.Show(message);
+        }
+    }
+}
