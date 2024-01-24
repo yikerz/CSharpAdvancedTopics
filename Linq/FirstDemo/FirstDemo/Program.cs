814e708acf8ab49d08551b3c496c4172a8236233
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-25 02:54:56 +1100

    First demo of Linq

---
+++ Linq/FirstDemo/FirstDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            OddNumbers(nums);
+            Console.ReadKey();
+        }
+
+        public static void OddNumbers(int[] nums)
+        {
+            IEnumerable<int> oddNums = from num in nums
+                                     where num % 2 != 0
+                                     select num;
+            foreach (int num in oddNums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
