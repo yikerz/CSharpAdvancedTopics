3957ca83f03220b262b4ef091fb31921518ff536
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-27 01:37:36 +1100

    Demo of interface usage

---
+++ OOP/InterfaceDemo/InterfaceDemo/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    // class extended from interface has to implement all methods of the interface
+    public class Circle : IShape
+    {
+        public int Radius { get; set; }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
---
+++ OOP/InterfaceDemo/InterfaceDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InterfaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(3);
+            double area = circle.CalculateArea();
+            Console.WriteLine(area);
+            Console.ReadKey();
+        }
+    }
+}
