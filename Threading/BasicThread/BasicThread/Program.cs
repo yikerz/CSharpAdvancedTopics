69742c21fbfa63cd159a17499044edab2737339e
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-28 11:17:31 +1100

    Demo of how to create a thread and start it

---
+++ Threading/BasicThread/BasicThread/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+
+namespace BasicThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello World 1");
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello World 2");
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello World 3");
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello World 4");
+            Thread.Sleep(1000);
+            */
+
+            Thread thread1 = new Thread(() =>
+            {
+                Console.WriteLine("Thread 1");
+                Thread.Sleep(1000);
+            });
+            Thread thread2 = new Thread(() =>
+            {
+                Console.WriteLine("Thread 2");
+                Thread.Sleep(1000);
+            });
+            Thread thread3 = new Thread(() =>
+            {
+                Console.WriteLine("Thread 3");
+                Thread.Sleep(1000);
+            });
+            Thread thread4 = new Thread(() =>
+            {
+                Console.WriteLine("Thread 4");
+                Thread.Sleep(1000);
+            });
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+        }
+    }
+}
