b5222b016c57a0f19a935a42a87b8da481dfd2ab
Travis LunMong <travis.lunmong@fdmgroup.com>
2024-01-29 18:30:01 +1100

    Demo of using thread pool and background thread

---
+++ Threading/ThreadPoolAndBackgroundThread/ThreadPoolAndBackgroundThread/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ThreadPoolAndBackgroundThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // normal thread with background setup
+            
+            new Thread(() =>
+            {
+                Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} is started...");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} is ended...");
+            })
+            { IsBackground = true }.Start();
+            
+
+            // thread pool
+            /*
+            Enumerable.Range(0, 100).ToList().ForEach(i =>
+            {
+                ThreadPool.QueueUserWorkItem((o) =>
+                {
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} is started...");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} is ended...");
+                });
+            });
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
